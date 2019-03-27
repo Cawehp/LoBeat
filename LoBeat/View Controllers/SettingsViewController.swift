fab123b0d06c708c4d8cc44f28eb51d14e09258d
Cawehp <cawehpassereh@hotmail.se>
2019-03-27 21:40:51 +0100

    Small fixes

--- LoBeat/View Controllers/SettingsViewController.swift
+++ LoBeat/View Controllers/SettingsViewController.swift
@@ -9,34 +9,60 @@
 import Foundation
 import UIKit
 
+//WHOLE CLASS UNDER CONSTRUCTION
+
 class SettingsViewController: UIViewController {
     
     
     override func viewDidLoad() {
-//        super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .black
         
         let backButton = UIButton()
+        let metronomeButton = UIButton()
+        
         backButton.backgroundColor = .white
+        metronomeButton.backgroundColor = .white
         
-        backButton.frame = CGRect(x: view.frame.width/2, y: view.frame.height, width: 50, height: 50)
+        metronomeButton.setTitle("Metronome \n On/Off", for: .normal)
+        metronomeButton.setTitleColor(.black, for: .normal)
         
-        backButton.center = self.view.center
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.black, for: .normal)
+        
+        metronomeButton.layer.cornerRadius = 25.0
+        backButton.layer.cornerRadius = 25.0
+        
+        backButton.frame = CGRect(x: view.frame.width/2 - 25 , y: view.frame.height - 100, width: 50, height: 50)
+        metronomeButton.frame = CGRect(x: view.frame.width/2 - 100 , y: view.frame.height/2 - 200, width: 200, height: 200)
         
         backButton.addTarget(self, action: #selector(backButtonPressed(_:)), for: .touchDown)
+        metronomeButton.addTarget(self, action: #selector(metronomeButtonPressed(_:)), for: .touchDown)
+        
         
         view.addSubview(backButton)
+        view.addSubview(metronomeButton)
         
     }
     
     
     @objc func backButtonPressed(_ sender: UIButton) {
         
+        self.dismiss(animated: true, completion: nil)
         
-        let drumView = DrumController()
-        self.present(drumView, animated: true, completion: nil)
         
+    }
+    
+    @objc func metronomeButtonPressed(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "METRO GNOME", message: "The metronome is under construction", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default,
+                                      handler: { _ in
+                                        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+
+        print("METRO GNOME UNDER CONSTRUCTION")
         
     }
     
